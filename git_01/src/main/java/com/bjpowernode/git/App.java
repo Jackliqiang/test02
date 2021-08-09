6b466d53685a9cd5379e031e16b666f0eedb4317
Jackliqiang <49141420+Jackliqiang@users.noreply.github.com>
2021-08-09 08:58:03 +0800

    04

--- git_01/src/main/java/com/bjpowernode/git/App.java
+++ git_01/src/main/java/com/bjpowernode/git/App.java
@@ -1,17 +1,13 @@
 package com.bjpowernode.git;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+/** Hello world! */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
 
     System.out.println("dddddddd");
 
     System.out.println("hhhhhhhddddd");
-    }
+    System.out.println("ddddddwwwsss");
+  }
 }
